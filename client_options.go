12368d65128b0d0bbd0c96f7cae96b669bb7923a
kubeall <kubeall@aliyun.com>
2022-07-15 14:32:54 +0800

    bug fix

--- client_options.go
+++ client_options.go
@@ -28,7 +28,7 @@ type ClientOptionFunc func(*Client) error
 // WithBaseURL sets the base URL for API requests to a custom endpoint.
 func WithBaseURL(urlStr string) ClientOptionFunc {
 	return func(c *Client) error {
-		return c.setBaseURL(urlStr)
+		return c.SetBaseURL(urlStr)
 	}
 }
 
